8a8ea470e610c8918acebc3103f09d214ac9792b
AkhileshNS <nsakhilesh02@gmail.com>
2018-07-06 22:21:33 +0530

    Added Get and Post routes

--- server/server.js
+++ server/server.js
@@ -1,3 +1,37 @@
+
+//External Libraries
+var express = require('express');
+var bodyParser = require('body-parser');
+
+//Internal Libraries
 var {mongoose} = require('./db/mongoose');
 var {User} = require('./models/user');
 var {Todo} = require('./models/todo');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    }) 
+});
+
+app.listen(3000, () => {
+    console.log(`Starting Server at port ${3000}`);
+});
